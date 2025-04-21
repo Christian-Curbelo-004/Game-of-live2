33320a09677d66f677807adec5f6bb79851ca302
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-04-21 20:22:58 -0300

    Trabajo hecho

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
+using System.Collections.Generic;
+
 namespace Ucu.Poo.GameOfLife
 {
     class Program
     {
-        //static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
 }
+// boardreadfile("board.txt");
