124a4e5343908385fb2bb9ea44f1f92147197c84
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-04-22 10:48:36 -0300

    board hecho

--- src/Program/Board.cs
+++ src/Program/Board.cs
@@ -6,22 +6,38 @@ namespace Ucu.Poo.GameOfLife;
 
 public class Board
 {
+    bool[,] board;
+
     public int height
     {
-        get { return height; }
-        set {}
+        get;
+        private set;
     }
+    
 
     public int width
     {
-        get { return width;}
-        set{}
+        get;
+        private set;
     }
     
     
-    public Board(int height, int width)
+    public Board(int height, int width, bool [,] board = null)
     {
         this.height = height;
         this.width = width;
+        this.board = new bool[width, height];
+        if (board !=null )
+        {
+            for (int i = 0; i < Math.Min(width, board.GetLength(dimension: 0)); i++)
+            {
+                for (int x = 0; x < Math.Min(height, board.GetLength(dimension: 1)); x++)
+                {
+                    this.board[i, x] = board[i, x];
+                }
+            }
+            
+        }
+
     }
 }
